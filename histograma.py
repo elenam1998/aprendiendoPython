d8fd2caa2204ee2e82e8e5028ca68bea3aaf9fcd
ELENA-MONTESANO\elena <elenamontesanomartin@gmail.com>
2020-07-24 13:45:35 +0200

    Histograma con la función estadistica de (media - 2* desv.estandar)

--- histograma.py
+++ histograma.py
@@ -2,88 +2,106 @@ import numpy
 import rasterio
 from rasterio.plot import show_hist
 import matplotlib.pyplot as plt
+from PIL import Image
 
 #CAMBIO EN EL DATATYPE DEL TIF
-from PIL import Image
-im = Image.open('NDVI_2019_1.tif')
-im1 = numpy.array(im).astype(numpy.float32)
-im2 = Image.fromarray(numpy.uint8(im1*255))
-im3 = Image.fromarray(numpy.float32(im2))
-#convert (para la media, minimo y máximo, trabaja de 0-1, que es en lo que está mi imagen .tif)
-imarray = numpy.array(im1)
-#convert (para la lista de pixeles que me va a dar la moda, trabaja de 0-255 y luego ya lo devolvemos a 0-1)
-imarray2 = numpy.array(im3)
-im2.show()
 
-#HISTOGRAMA
+def TiffToNumpyArrays(OpenedImage):
+    imgArrayFloat32 = numpy.array(OpenedImage).astype(numpy.float32)
+    imgArrayUINT8 = Image.fromarray(numpy.uint8(imgArrayFloat32*255))
+    imgFloat32 = Image.fromarray(numpy.float32(imgArrayUINT8))
+    # convert (para la media, minimo y máximo, trabaja de 0-1, que es en lo que está mi imagen .tif)
+    imgArray_1 = numpy.array(imgArrayFloat32)
+    #convert (para la lista de pixeles que me va a dar la moda, trabaja de 0-255 y luego ya lo devolvemos a 0-1)
+    imgArray_255 = numpy.array(imgFloat32)
+    #imgArrayUINT8.show()
+    return imgArray_1, imgArray_255
 
-#bins son las separaciones
-#lw es el ancho de línea, stacked false es para que no se apilen unos encima de otro,Alpha y steptfilled
-# van vinculados, el segundo es para superponer histogramas por si hay mas de uno y el 
-# alpha es el grado de transparencia
-src=rasterio.open('NDVI_2019_1.tif')
 
-sep= int(input("Introduzca 0 si quiere separaciones fijas o marque 1 si quiere porcentuales para el histograma "))
+def calculateHistogramAndParameters(imagePath):
+    OpenedImage = Image.open(imagePath)
+    RasterioOpenedImage = rasterio.open(imagePath)
 
-#separaciones fijas
-if sep == 0:
-    s= float(input("¿Cada cuanto quiere las separaciones? "))
-    div=(numpy.nanmax(imarray)-numpy.nanmin(imarray))//s
-    div2=round(div,2)
-    div3=int(div2)
-    #Para guardar el histograma en png
-    fig, axh = plt.subplots()
-    show_hist (src, bins=div3, lw=0.0, stacked=False, alpha=0.3, histtype='stepfilled', title="Histogram", ax=axh)
-    fig.savefig('Histograma.png')
+    sep = int(input("Introduzca 0 si quiere separaciones fijas o marque 1 si quiere porcentuales para el histograma "))
+    imgArray_1, imgArray_255 = TiffToNumpyArrays(OpenedImage)
 
-#separaciones porcentuales
-if sep == 1:
-    nm= int(input("¿Cúantas separaciones quiere en el histograma? "))
-     #Para guardar el histograma en png
-    fig, axh = plt.subplots()
-    show_hist( src, bins=nm, lw=0.0, stacked=False, alpha=0.3, histtype='stepfilled', title="Histogram", ax=axh)
-    fig.savefig('Histograma.png')
+    #para el histograma
+    #bins son las separaciones
+    #lw es el ancho de línea, stacked false es para que no se apilen unos encima de otro,Alpha y steptfilled
+    # van vinculados, el segundo es para superponer histogramas por si hay mas de uno y el
+    # alpha es el grado de transparencia
+
+    #separaciones fijas
+    if sep == 0:
+        s = float(input("¿Cada cuanto quiere las separaciones? "))
+        div = (numpy.nanmax(imgArray_1)-numpy.nanmin(imgArray_1))//s
+        div2 = round(div, 2)
+        div3 = int(div2)
+        #Para guardar el histograma en png
+        fig, axh = plt.subplots()
+        show_hist(RasterioOpenedImage, bins=div3, lw=0.0, stacked=False, alpha=0.3,
+                  histtype='stepfilled', title="Histogram", ax=axh)
+        fig.savefig('Histograma.png')
+        #separaciones porcentuales
+    elif sep == 1:
+        nm = int(input("¿Cúantas separaciones quiere en el histograma? "))
+        #Para guardar el histograma en png
+        fig, axh = plt.subplots()
+        show_hist(RasterioOpenedImage, bins=nm, lw=0.0, stacked=False, alpha=0.3,
+                  histtype='stepfilled', title="Histogram", ax=axh)
+        fig.savefig('Histograma.png')
     
+    #SACAR MODA
+    listaPixeles = list()
+    for row in imgArray_255:
+        for cell in row:
+            if cell > 0.0:
+                listaPixeles.append(cell)
+
+    #FILTRADO CON LA FÓRMULA --> MEDIA - 2*DESV.ESTANDAR
+    minimo      = numpy.nanmin(imgArray_1)
+    maximo      = numpy.nanmax(imgArray_1)
+    media       = numpy.nanmean(imgArray_1)
+    desviacion  = numpy.nanstd(imgArray_1)
+    moda = max(set(listaPixeles), key=listaPixeles.count)/255
+    Kcp = numpy.nanmean(imgArray_1)*1.25
+    Kc = Kcp + 0.1
+
+    return minimo, maximo, media, desviacion, moda, Kc, imgArray_1, imgArray_255
+
+
+#############################################################
+#Main code
+
+
+minValue, maxValue, average, std, mod, kc, ImgArray_0_1, ImgArray_0_255 = calculateHistogramAndParameters(
+    'NDVI_2019_1.tif')
+
 
 
 #SACAR MINIMO, MAXIMO Y MEDIA (ignoramos los valores nan, si trabajasemos de 0-255 los nan se convierten en 0.0)
-print("MINIMO",numpy.nanmin(imarray)) 
-print("MAXIMO", numpy.nanmax(imarray))
-print("MEDIA", numpy.nanmean(imarray))
-imarray.shape
-print("TAM", imarray.shape)
+print("MINIMO", minValue)
+print("MAXIMO", maxValue)
+print("MEDIA", average)
+print("DESVIACIÓN ESTANDAR", std)
+print("TAM", ImgArray_0_1.shape)
 
-#SACAR MODA
-listaPixeles=list()
-for row in imarray2:
-    for cell in row:
-        if cell > 0.0:
-            listaPixeles.append(cell)        
-print("La moda es:")
-print(max(set(listaPixeles), key = listaPixeles.count)/255)
-
-#CALCULO DE LA KC
-Kcp = numpy.nanmean(imarray)*1.25
-Kc = Kcp + 0.1
-print("La Kc es de ", Kc)
-
-#20% POR DEBAJO DE LA MODA 
-descarta=(max(set(listaPixeles), key = listaPixeles.count)/255)*0.8
+#descarta = mod*0.8
+descarta = average - 2*std
 print("descarta",descarta)
-
 #descarto los mayores 
-listaPixeles2=list()
-for row in imarray:
+listaPixeles=list()
+for row in ImgArray_0_1:
     for cell in row:
         if cell > descarta:
             cell=0.0
-            listaPixeles2.append(cell) 
+            listaPixeles.append(cell) 
         else:
-            listaPixeles2.append(cell) 
+            listaPixeles.append(cell) 
         #print(cell, end=' ')
 
-imagenVector=numpy.asarray(listaPixeles2,dtype=numpy.float32) 
-imagenVector2=imagenVector.reshape(imarray.shape)
+imagenVector=numpy.asarray(listaPixeles,dtype=numpy.float32) 
+imagenVector2 = imagenVector.reshape(ImgArray_0_1.shape)
 imfin = Image.fromarray(numpy.uint8(imagenVector2*255))
 imfin2 = Image.fromarray(numpy.float32(imfin))
 #los valores totalmente negros (0.0) son los que son correctos, hay que mejorar los otros
